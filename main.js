cfeda0dc9b588063ba275d639164b07be6ba9dae
lebrancconvas <lebrancconvas@gmail.com>
2022-07-06 22:45:46 +0700

    💬 Add Text to Website.

--- main.js
+++ main.js
@@ -3,4 +3,5 @@ import './style.css'
 document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+  <div>Hello MBC#2. I'm Poom, Nice to meet you.</div>
 `
